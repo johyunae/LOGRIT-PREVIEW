a946403ac808f8b882ca95257a04bc2675a8a8a0
johyunae <heman333@kakao.com>
2026-09-16 16:18:17 +0900

    Add preview app support files

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE_NAME = 'logrit-preview';
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (e) => e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key)))).then(() => self.clients.claim())));
+self.addEventListener('fetch', (e) => e.respondWith(fetch(e.request)));
